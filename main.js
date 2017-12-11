d78fb55e72f72992b4ed41b9dbccc3142b713e7f
Lokesh Jadhav <lokesh.jadhav10@gmail.com>
2017-12-11 13:49:29 +0100

    complete

---
+++ main.js
@@ -0,0 +1,9 @@
+var tree = new Tree();
+for ( var i = 0; i < 10; i++ ) {
+  tree.add(Math.floor(Math.random() * 100) + 1);
+}
+// tree.add(4);
+// tree.add(3);
+// tree.add(5);
+// tree.add(1);
+console.log(tree);
